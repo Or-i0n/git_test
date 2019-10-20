01493632b232b3a3f1d1a6d97e63382f4ad9fbb2
Ori-i0n <orionconner00@gmail.com>
2019-10-20 14:17:48 +0530

    Added new function: prime_range()

--- odd_even.py
+++ odd_even.py
@@ -18,4 +18,17 @@ def prime(num):
                 return False
         return True
 
-print(prime(0))
+
+def prime_range(start, stop=None, step=1):
+    """Return a generator for printing Prime Numbers in a range."""
+    if not stop:
+        stop, start = start, 0
+
+    for i in range(start, stop, step):
+        if prime(i):
+            yield i
+
+
+# Testing prime_range()
+for i in prime_range(10, 20):
+    print(i)
